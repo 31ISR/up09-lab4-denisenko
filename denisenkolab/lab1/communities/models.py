931b1a7697215b4be458fc2ceed79a38325f63db
ST3609 <ST3609@TE.local>
2024-12-02 12:32:35 +0600

    task2

--- denisenkolab/lab1/communities/models.py
+++ denisenkolab/lab1/communities/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Post(models.Model):
+    name = models.TextField(max_length=75)
+    description = models.TextField(max_length=150)
+    slug = models.SlugField()
+    date = models.DateTimeField(auto_now_add=True)
+    free = models.BooleanField()
+
+    def __str__(self):
+        return self.title
 # Create your models here.
