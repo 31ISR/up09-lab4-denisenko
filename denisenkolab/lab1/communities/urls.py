236d0d65da271b7be69136288c94312708c147f8
anime <anime@len.com>
2024-12-09 15:39:35 +0600

    finish

--- denisenkolab/lab1/communities/urls.py
+++ denisenkolab/lab1/communities/urls.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-app_name = 'community'
+app_name = 'communities'
 
 urlpatterns = [
     path('', views.communities_list, name="list"),
-    path('<slug:slug>', views.community_page, name="page")
+    path('<slug:slug>', views.communities_page, name="page")
 ]
--- denisenkolab/lab1/communities/views.py
+++ denisenkolab/lab1/communities/views.py
@@ -5,7 +5,7 @@ def communities_list(req):
     communities = Community.objects.all().order_by('-date')
     return render(req, 'communities/communities_list.html',  {'communities': communities})
 
-def community_page(request, slug):
-    communities = Community.objects.get(slug=slug)
-    return render(request, 'communities/community_page.html', {'communities': communities})
+def communities_page(request, slug):
+    community = Community.objects.get(slug=slug)
+    return render(request, 'communities/communities_page.html', {'community': community})
 # Create your views here.
