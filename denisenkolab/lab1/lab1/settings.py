7a3ec690ef0eb1558865b50acd76c27cfab6c9ad
anime <anime@len.com>
2024-12-09 17:42:54 +0600

    task3

--- denisenkolab/lab1/lab1/settings.py
+++ denisenkolab/lab1/lab1/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'posts',
-    'communities'
+    'communities',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR)
