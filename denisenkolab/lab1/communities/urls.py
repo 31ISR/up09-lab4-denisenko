5b0ca088334db651e47f88ee8bc89d9046c325e9
ST3612 <ST3612@TE.local>
2024-12-18 10:10:39 +0600

    doesnt log in

--- denisenkolab/lab1/communities/urls.py
+++ denisenkolab/lab1/communities/urls.py
@@ -6,5 +6,6 @@ app_name = 'communities'
 
 urlpatterns = [
     path('', views.communities_list, name="list"),
+    path('new-post/', views.post_new, name="new-post"),
     path('<slug:slug>', views.communities_page, name="page")
 ]
--- denisenkolab/lab1/communities/views.py
+++ denisenkolab/lab1/communities/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Community
+from django.contrib.auth.decorators import login_required
 
 def communities_list(req):
     communities = Community.objects.all().order_by('-date')
@@ -9,3 +10,8 @@ def communities_page(request, slug):
     community = Community.objects.get(slug=slug)
     return render(request, 'communities/communities_page.html', {'community': community})
 # Create your views here.
+
+
+@login_required(login_url="/users/login/")
+def post_new(request):
+    return render(request, 'communities/community_new.html')
